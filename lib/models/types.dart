f5e8763c4395622c15e100813ae6e52aa7788052
= <george.bentes@gmail.com>
2023-11-07 22:46:35 -0300

    add: single source of truth for clients and for client types

---
+++ lib/models/types.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'client_type.dart';
+
+class Types extends ChangeNotifier{
+  List<ClientType> types;
+
+  Types({required this.types});
+
+  void add(ClientType type){
+    types.add(type);
+    notifyListeners();
+  }
+}
